3a125032728bf2afea2ea44282d037dc37edab48
koeuk kos <koeukkos@gmail.com>
2026-07-18 17:58:59 +0700

    style: fix indentation of exception handling comment blocks across controllers

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -105,7 +105,7 @@ class CategoryController extends Controller
         } catch (\Exception $e) {
             DB::rollback();
 
-// getMessage() on a QueryException is the SQLSTATE, the whole
+            // getMessage() on a QueryException is the SQLSTATE, the whole
             // parameterised query and its bound values. That is a log entry,
             // not something to flash at whoever clicked the button.
             report($e);
@@ -129,7 +129,7 @@ class CategoryController extends Controller
         } catch (\Exception $e) {
             DB::rollback();
 
-// getMessage() on a QueryException is the SQLSTATE, the whole
+            // getMessage() on a QueryException is the SQLSTATE, the whole
             // parameterised query and its bound values. That is a log entry,
             // not something to flash at whoever clicked the button.
             report($e);
@@ -161,7 +161,7 @@ class CategoryController extends Controller
         } catch (\Exception $e) {
             DB::rollback();
 
-// getMessage() on a QueryException is the SQLSTATE, the whole
+            // getMessage() on a QueryException is the SQLSTATE, the whole
             // parameterised query and its bound values. That is a log entry,
             // not something to flash at whoever clicked the button.
             report($e);
